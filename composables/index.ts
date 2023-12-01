701fa44ff2fb5a5a359077846aee22ffca4fcbc9
Kathryn Luo <kathryn.luo.0331@gmail.com>
2023-12-01 17:32:04 +0800

    feat: 調整 avatar imgurl 資料

--- composables/index.ts
+++ composables/index.ts
@@ -9,10 +9,12 @@ export const urlHttpToHttps = (url: string) => {
 export const getFixedImageUrlFromFirestore = (imageUrl: string): string => {
   // 為了修正 Nuxt/image 加上 domains 後產生的問題
   // TODO: 確認後續版本是否修正，並跟著調整
+  const config = useRuntimeConfig()
+
   if (imageUrl && imageUrl.indexOf('http') === -1) return imageUrl
-  const splicedImgUrl = imageUrl.replace('https://firebasestorage.googleapis.com/v0/b/read-passbook.appspot.com/o/', '')
+  const splicedImgUrl = imageUrl.replace(config.public.firestoreImagePrefixUrl, '')
   const encodedImagePath = encodeURIComponent(splicedImgUrl)
-  const fixedUrl = 'https://firebasestorage.googleapis.com/v0/b/read-passbook.appspot.com/o/' + encodedImagePath
+  const fixedUrl = config.public.firestoreImagePrefixUrl + encodedImagePath
   return fixedUrl
 }
 
